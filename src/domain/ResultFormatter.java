64684b24308edab377f6ee4d867e1924cc4ab07a
yfurukawa <yo4.furukawa@gmail.com>
2016-01-03 23:44:30 +0900

    検索結果のリストに詳細ボタンを追加

--- src/domain/ResultFormatter.java
+++ src/domain/ResultFormatter.java
@@ -17,14 +17,28 @@ public class ResultFormatter {
      */
     public List<String> buildFormat(List<Book> book) {
         List<String> result = new ArrayList<String>();
-        result.add("<TD>");
+        result.add("<table>");
+        result.add("<thead>");
+        result.add("<tr>");
+        result.add("<th>ISBN番号</th>");
+        result.add("<th>書籍名</th>");
+        result.add("<th>著者名</th>");
+        result.add("<th>価格</th>");
+        result.add("<th></th>");
+        result.add("</tr>");
+        result.add("</thead>");
+        result.add("<tbody>");
         for(int i = 0; i < book.size(); ++i) {
-            result.add("<TR>" + book.get(i).getIsbn() + "</TR>");
-            result.add("<TR>" + book.get(i).getName() + "</TR>");
-            result.add("<TR>" + book.get(i).getAuthor() + "</TR>");
-            result.add("<TR>" + book.get(i).getPrice().toString() + "</TR>");
+            result.add("<tr>");
+            result.add("<td>" + book.get(i).getIsbn() + "</td>");
+            result.add("<td>" + book.get(i).getName() + "</td>");
+            result.add("<td>" + book.get(i).getAuthor() + "</td>");
+            result.add("<td>" + book.get(i).getPrice().toString() + "</td>");
+            result.add("<td><input type=\"submit\" value=\"詳細\"></td>");
+            result.add("</tr>");
         }
-        result.add("</TD>");
+        result.add("</tbody>");
+        result.add("</table>");
         return result;
         
     }
