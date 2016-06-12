bcce2c21b6ff8fada20904cd61f280ae5778dc40
yfurukawa <yo4.furukawa@gmail.com>
2016-06-12 22:01:06 +0900

    ファイルアップロードの更新

--- src/view/FileUploadBean.java
+++ src/view/FileUploadBean.java
@@ -3,10 +3,17 @@
  */
 package view;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
- 
+
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
  
@@ -15,8 +22,9 @@ import org.primefaces.model.UploadedFile;
  *
  */
 @ManagedBean
+@RequestScoped
 public class FileUploadBean {
-
+    private String destination="/tmp/";
     private UploadedFile file;
     
     /**
@@ -37,9 +45,52 @@ public class FileUploadBean {
      * 
      */
     public void upload() {
-        if(file != null) {
+        if(this.file != null) {
             FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
+    }
+    
+    /**
+     * @param event
+     */
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file        
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }  
+ 
+    /**
+     * @param fileName
+     * @param in
+     */
+    public void copyFile(String fileName, InputStream in) {
+           try {
+              
+              
+                // write the inputStream to a FileOutputStream
+                OutputStream out = new FileOutputStream(new File(destination + fileName));
+              
+                int read = 0;
+                byte[] bytes = new byte[1024];
+              
+                while ((read = in.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+              
+                in.close();
+                out.flush();
+                out.close();
+              
+                System.out.println("New file created!");
+                } catch (IOException e) {
+                System.out.println(e.getMessage());
+                }
     }
 }
